5e156ae2fac3e02e2e3112126a112065512283fe
Marco Mernberger <marco.mernberger@uni-marburg.de>
2022-06-14 10:45:34 +0200

    vep

--- VEP/flake.nix
+++ VEP/flake.nix
@@ -37,6 +37,13 @@
             autoPatchelfHook
             zlib
           ];
+          buildPhase = ''
+          '';
+          installPhase = ''
+            cd ensembl-vep
+            perl INSTALL.pl
+          '';
+
         });
     };
 }
